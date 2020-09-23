d26f9940d7e19f05ebda51b22fed7059b525abbd
saeed <saeedalalwi999@gmail.com>
2020-09-23 13:36:57 +0400

    first commit

---
+++ std_porfile/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StdPorfileConfig(AppConfig):
+    name = 'std_porfile'
---
+++ std_porfile/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from models import models
+
+# Create your models here.
+
+
+class std_profile(models.Model):
+    pass
+
